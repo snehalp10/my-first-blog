13323273656f571a61a95a8334f2f2765f013448
snehal <snehalp1003@gmail.com>
2015-09-12 14:55:59 +0530

    My Django Girls app, first commit

---
+++ first.py
@@ -0,0 +1,29 @@
+'''print('Hello World')
+if 5>2:
+	print('It works')
+name = 'snehal'
+if(name=='swara'):
+	print('Hey swara!')
+elif(name=='nain'):
+	print('Hey nain!')
+else:
+	print('Hey snehal!')
+
+def first():
+	print('This is my first function')
+first()
+
+def second(c):
+	a=9
+	b=3
+	c=a/b
+	print(c)
+second(1)
+'''
+def name(n):
+	print('Hi '+n+' !')
+#name("Snehal")
+	girls = ['Rachel', 'Monica', 'Phoebe', 'Ola', 'You']
+	for name in girls:
+		hi(name)
+		print('Next girl')
